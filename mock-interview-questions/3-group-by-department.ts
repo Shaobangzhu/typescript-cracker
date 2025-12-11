e3a3981fdfed77714223791dd931dc4b75e56625
Chaoran Lu <andyluloveusc@gmail.com>
2025-12-11 15:55:21 -0800

    zoom interview mock Q3

---
+++ mock-interview-questions/3-group-by-department.ts
@@ -0,0 +1,22 @@
+const students = [
+    { id: 1, department: "CS" },
+    { id: 2, department: "Math" },
+    { id: 3, department: "CS" },
+];
+
+function groupBy<T, K extends string | number | symbol>(
+    items: T[],
+    getKey: (item: T) => K
+): Record<K, T[]> {
+    const result = {} as Record<K, T[]>;
+
+    for(const item of items){
+        const key = getKey(item);
+        if (!result[key]) {
+            result[key] = [];
+        }
+        result[key].push(item);
+    }
+
+    return result;
+}
