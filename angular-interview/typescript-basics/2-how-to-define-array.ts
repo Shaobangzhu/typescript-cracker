20fc091681beb34bb5ff4b67d0fa2d18a9651b19
Chaoran Lu <andyluloveusc@gmail.com>
2026-01-23 15:29:17 -0800

    docs: remove angular related contents

--- angular-interview/typescript-basics/2-how-to-define-array.ts
+++
@@ -1,9 +0,0 @@
-// - How to define an array in TypeScript? Show all ways.
-// - How to define an array of numbers and strings.
-
-{
-    let fruites1: string[] = ['Apple', 'Orange', 'Banana'];
-    let fruites2: Array<string> = ['Peach', 'Watermelon', 'grapes'];
-
-    let values: (string | number)[] = ['Apple', 2, 'Orange', 3];
-}
--- angular-interview/typescript-basics/3-type-assertion.ts
+++
@@ -1,8 +0,0 @@
-// What is type assertion
-// How to fix this code?
-
-{
-    const getItem = (item: number | undefined) => {
-        return item?.toString();
-    }
-}
